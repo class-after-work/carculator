68b081eed841dd44ae29601d13ad237e47303fdb
Guheon2 <131875839+Guheon2@users.noreply.github.com>
2023-07-03 22:17:43 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const carculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  powerof: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+carculator.add(2, 4);
+carculator.minus(5, 4);
+carculator.divide(10, 2);
+carculator.powerof(2, 2);
